49546bf79f773f5be806543f16eab0455787fe66
Francis Yao <neko.makie27@gmail.com>
2021-05-24 10:08:37 -0700

    Added app.js script in index

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log('app.js is loaded!')
+
+//####### Model #######
+/*
+Store data in 3 x 3 grid
+*/
+
+//####### View #######
+/*
+Display Grid
+Display X O
+Display Refresh Button
+*/
+
+//####### Controller #######
+/*
+Handles X and O input
+Handles refresh page button input
+*/
